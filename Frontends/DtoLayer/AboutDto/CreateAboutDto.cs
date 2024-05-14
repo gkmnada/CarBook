162884075b7431c09df853ab2be5c7b0bef36e1f
gkmnada <gkmenada@hotmail.com>
2024-05-14 11:17:24 +0300

    CARBOOK

---
+++ Frontends/DtoLayer/AboutDto/CreateAboutDto.cs
@@ -0,0 +1,9 @@
+﻿namespace DtoLayer.AboutDto
+{
+    public class CreateAboutDto
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+    }
+}
--- Frontends/PresentationUI/Controllers/AboutController.cs
+++ Frontends/PresentationUI/Controllers/AboutController.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DtoLayer.AboutDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace PresentationUI.Controllers
 {
     public class AboutController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _clientFactory;
+
+        public AboutController(IHttpClientFactory clientFactory)
         {
+            _clientFactory = clientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _clientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7210/api/About");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultAboutDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
