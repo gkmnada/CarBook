fe1036711cf791a5b4b7be291712af9614677d9f
gkmnada <gkmenada@hotmail.com>
2024-05-18 18:40:14 +0300

    CARBOOK

--- Frontends/PresentationUI/Controllers/ServicesController.cs
+++ Frontends/PresentationUI/Controllers/ServicesController.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DtoLayer.ServiceDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace PresentationUI.Controllers
 {
     public class ServicesController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _clientFactory;
+
+        public ServicesController(IHttpClientFactory clientFactory)
         {
+            _clientFactory = clientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _clientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7210/api/Service");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultServiceDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
